1391a7290eb823a6af49548f94af1b8c9959b7ee
Niall <niallmelvin@alumnircsi.com>
2025-02-05 21:31:15 +0000

    Initial commit

---
+++ aurarette/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for aurarette project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aurarette.settings')
+
+application = get_asgi_application()
