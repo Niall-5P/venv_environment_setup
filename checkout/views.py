f9521c349e3d6e1a79f7e530965055f06ffd619b
Niall <niallmelvin@alumnircsi.com>
2025-03-03 11:15:38 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QyVszR5oY7UpMNEvQxhyxxy4mgF2WcH5tiwpS3xAjGZnn6fbKs8xx2FhW09BZrYqPq8op1z368Hj1SAh0y8CcKb00jkZwsDbD',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
