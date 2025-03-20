8566ec0895fa9e23d6d1cdf43e1c876a5d98c775
Niall <niallmelvin@alumnircsi.com>
2025-03-20 09:43:38 +0000

    Update environment variable

--- env.py
+++ env.py
@@ -3,3 +3,4 @@ import os
 os.environ.setdefault('MY_ENVIRONMENT_VARIABLE', 'variable_value')
 os.environ.setdefault('STRIPE_WH_SECRET', 'whsec_a077813046652874ca8ca977c2e551f0cb8df2b66d5955a4dc5e8f9e4d5c0ef0')
 os.environ.setdefault('SECRET_KEY', 'django-insecure-e1+ekbxai#0!$=1bvuq2_+zl1k1eolov13oovi&yphsdl*5lvh')
+os.environ.setdefault('DEVELOPMENT', '1')
