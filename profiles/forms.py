4d827652e738e64019279c69d1d31e78fdd65b08
Niall <niallmelvin@alumnircsi.com>
2025-04-24 21:20:02 +0100

    fix django countries bug

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,34 +1,45 @@
+# profiles/forms.py
 from django import forms
+from django_countries import countries
 from .models import UserProfile
 
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        exclude = ('user',)
+        exclude = ("user",)
 
     def __init__(self, *args, **kwargs):
-        """
-        Add placeholders and classes, remove auto-generated
-        labels and set autofocus on first field
-        """
         super().__init__(*args, **kwargs)
+
+        # ---- hard-materialise the country choices --------------------
+        country_field = self.fields["default_country"]
+
+        # build a regular (value, label) list – add the blank option first
+        concrete_choices = (("", "---------"),) + tuple(countries)
+
+        # assign it to both the field *and* its widget
+        country_field.choices = concrete_choices
+        country_field.widget.choices = concrete_choices
+        # ----------------------------------------------------------------
+
         placeholders = {
-            'default_phone_number': 'Phone Number',
-            'default_postcode': 'Postal Code',
-            'default_town_or_city': 'Town or City',
-            'default_street_address1': 'Street Address 1',
-            'default_street_address2': 'Street Address 2',
-            'default_county': 'County, State or Locality',
+            "default_phone_number": "Phone Number",
+            "default_postcode": "Postal Code",
+            "default_town_or_city": "Town or City",
+            "default_street_address1": "Street Address 1",
+            "default_street_address2": "Street Address 2",
+            "default_county": "County, State or Locality",
         }
 
-        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
-        for field in self.fields:
-            if field != 'default_country':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
-                self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
-            self.fields[field].label = False
+        self.fields["default_phone_number"].widget.attrs["autofocus"] = True
+        for name, field in self.fields.items():
+            if name != "default_country":
+                ph = placeholders.get(name, "")
+                if field.required:
+                    ph += " *"
+                field.widget.attrs["placeholder"] = ph
+            field.widget.attrs["class"] = (
+                "border-black rounded-0 profile-form-input"
+            )
+            field.label = False
